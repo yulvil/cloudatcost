be1909177e9cbcf7f2c2172f944a6f01ff731dd5
yulvil <yulvil@users.noreply.github.com>
2016-12-07 12:20:45 -0800

    Initial revision

---
+++ root.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NEWUSER=$1
+
+deluser --remove-home user
+
+adduser --disabled-password $NEWUSER
+# Grant sudo access to the new user
+adduser $NEWUSER sudo
+echo "%sudo ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+sudo -k
