7dd2d061d1107e3d8771905155a83943d1fea14d
yulvil <yulvil@users.noreply.github.com>
2016-12-07 13:33:02 -0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,9 +4,11 @@ set -x
 
 NEWUSER=$1
 
+# remove default user
 deluser --remove-home user
 
 adduser --disabled-password --gecos "" $NEWUSER
+su -c 'mkdir ~/.ssh/; chmod 700 ~/.ssh/' $NEWUSER
 
 # Password will be disabled in the next step
 echo "$NEWUSER:qwer1234" | chpasswd
