d367e9159866c20ffbf2f60f338589220d31a6e7
yulvil <yulvil@users.noreply.github.com>
2016-12-07 15:15:04 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z $CACIP || -z $CACUSER ]]; then
+  echo "Usage..."
+  exit 1
+fi
+
+ssh root@$CACIP 'curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/root.sh | bash -s $CACUSER'
+
+CACPWD=$(cat /proc/sys/kernel/random/uuid)
+echo $CACPWD
+curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/user_local.sh | bash -s $CACIP $CACUSER $CACPWD
+
+ssh $CACUSER@$CACIP 'curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/user.sh | bash -s'
