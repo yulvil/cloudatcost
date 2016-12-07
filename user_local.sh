43f3bd12bcbc36a578a90b0e85dda8e59fe540d7
yulvil <yulvil@users.noreply.github.com>
2016-12-07 12:44:33 -0800

    Update user_local.sh

--- user_local.sh
+++ user_local.sh
@@ -4,7 +4,7 @@ SERVERNAME=$1
 IP=$2
 UNAME=$3
 
-ssh-keygen -t rsa -b 4096 -c "$SERVERNAME $IP" -f ~/.ssh/id_rsa_$SERVERNAME
+ssh-keygen -t rsa -b 4096 -C "$SERVERNAME $IP" -f ~/.ssh/id_rsa_$SERVERNAME
 scp ~/.ssh/id_rsa_$SERVERNAME.pub ${UNAME}@${IP}:
 
 ssh -i ~/.ssh/id_rsa_$SERVERNAME ${UNAME}@${IP}
