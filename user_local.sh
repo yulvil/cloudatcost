0b62cb1ed0914738ac281db4d74a5d931e58183d
yulvil <yulvil@users.noreply.github.com>
2016-12-07 14:00:59 -0800

    Update user_local.sh

--- user_local.sh
+++ user_local.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-SNAME=$1
-IP=$2
-UNAME=$3
-NEWPWD=$4
+IP=$1
+UNAME=$2
+NEWPWD=$3
 
-ssh-keygen -t rsa -b 4096 -C "$SNAME $IP" -f "~/.ssh/id_rsa_$SNAME" -N ${NEWPWD}
-scp ~/.ssh/id_rsa_$SERVERNAME.pub ${UNAME}@${IP}:
+if [[ -z $IP || -z $UNAME || -z $NEWPWD ]]; then
+        echo Usage...
+        exit 1
+fi
 
-ssh -t -t -i ~/.ssh/id_rsa_$SERVERNAME ${UNAME}@${IP}
+ssh-keygen -t rsa -b 4096 -C "$IP" -f ~/.ssh/id_rsa_$$ -N ${NEWPWD}
+scp -o PubkeyAuthentication=no ~/.ssh/id_rsa_$$.pub ${UNAME}@${IP}:~/.ssh/authorized_keys
+
+echo ssh -i ~/.ssh/id_rsa_$$ ${UNAME}@${IP}
