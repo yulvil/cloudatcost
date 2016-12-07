c421bd309cf92c5744f0879b6efb49fe5c754551
yulvil <yulvil@users.noreply.github.com>
2016-12-07 13:18:26 -0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,6 +7,10 @@ NEWUSER=$1
 deluser --remove-home user
 
 adduser --disabled-password --gecos "" $NEWUSER
+
+# Password will be disabled in the next step
+echo "$NEWUSER:qwer1234" | chpasswd
+
 # Grant sudo access to the new user
 adduser $NEWUSER sudo
 echo "%sudo ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
