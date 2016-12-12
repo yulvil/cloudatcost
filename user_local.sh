5bc23a45c2cd10c4ba01c9c4d4cb1f5a8bcb2a23
yulvil <yulvil@users.noreply.github.com>
2016-12-12 14:49:52 -0800

    Update user_local.sh

--- user_local.sh
+++ user_local.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 IP=$1
-UNAME=$2
-NEWPWD=$3
+SUBDOMAIN=$2
+UNAME=$3
+NEWPWD=$4
 
-if [[ -z $IP || -z $UNAME || -z $NEWPWD ]]; then
+if [[ -z $IP || -z $SUBDOMAIN || -z $UNAME || -z $NEWPWD ]]; then
         echo Usage...
         exit 1
 fi
 
-ssh-keygen -t rsa -b 4096 -C "$IP" -f ~/.ssh/id_rsa_$$ -N ${NEWPWD}
-scp -o PubkeyAuthentication=no ~/.ssh/id_rsa_$$.pub ${UNAME}@${IP}:~/.ssh/authorized_keys
+ssh-keygen -t rsa -b 4096 -C "${IP}" -f ~/.ssh/id_rsa_${SUBDOMAIN} -N ${NEWPWD}
+scp -o PubkeyAuthentication=no ~/.ssh/id_rsa_${SUBDOMAIN}.pub ${UNAME}@${IP}:~/.ssh/authorized_keys
 
 echo ssh -i ~/.ssh/id_rsa_$$ ${UNAME}@${IP}
