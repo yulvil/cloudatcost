f5c7148865cbf0108e73f8132f7a430fe6f30e6d
yulvil <yulvil@users.noreply.github.com>
2016-12-14 09:24:37 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [[ -z $CACIP || -z $CACUSER || -z $CACSUBDOMAIN ]]; then
   exit 1
 fi
 
-ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no root@$CACIP "curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/root.sh | bash -s \"$CACUSER\""
+ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no root@$CACIP "apt-get install -y curl; curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/root.sh | bash -s \"$CACUSER\""
 if [[ $? -ne 0 ]]; then
   exit $?
 fi
